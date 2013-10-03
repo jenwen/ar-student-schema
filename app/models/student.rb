79cb5ad91826cee0eb63a8a7dcb548755ceae502
Apprentice <apprentice@dbc08.local>
2013-10-02 19:28:41 -0700

    validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,13 +1,22 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
+  validates :email, :uniqueness => true
+  validates :email, :format => { :with => /\w+@{1}\w+\.\w{2,}/, :message => "Invalid email address"}
+  validates :age, :numericality => {:greater_than => 5}
+
+  ### NOT PASSING THIS VALIDATION ###
+  # validates :phone, :format => { :with => /\d{3}-\d{3}-\d{4}/, :message => "Invalid phone"}
+
+
+  
 
   def name
     "#{first_name} #{last_name}"
   end
 
   def age
-    Time.now.year - birthday.year
+    @age = Time.now.year - birthday.year
   end
 
 end
